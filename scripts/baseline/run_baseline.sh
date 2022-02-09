4892d9e08c8a8063354a48501728930883fe1fde
chrhjoh <christian.holm.johansen@gmail.com>
2022-02-09 11:40:39 +0100

    run_baseline run locally

--- scripts/baseline/run_baseline.sh
+++ scripts/baseline/run_baseline.sh
@@ -1,11 +1,11 @@
-cd /home/projects/vaccine/people/chrhol/speciale
+cd /Users/christianjohansen/Desktop/speciale
 # Input 1 is file 
 # input 2 in partition for test
 mkdir data/tmp
 
 grep "^P$2" $1 | cut -f2,3,4 > data/tmp/test.input
-grep "^P$2" $1 | cut -f2,3,4 > data/tmp/database.input
+grep -v "^P$2" $1 | cut -f2,3,4 > data/tmp/database.input
 
-../../morni/bin/pairlistscore_db_kernel data/tmp/test.input data/tmp/database.input >> results/baseline.res
+#tools/pairlistscore_kernel -blf data/blosum/BLOSUM50 -blqij data/blosum/blosum62.qij data/tmp/test.input data/tmp/database.input >> results/baseline.res
 
-rm -r data/tmp
+#rm -r data/tmp
